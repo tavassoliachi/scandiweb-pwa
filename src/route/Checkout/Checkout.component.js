bf5129f954a91543df5900c6648162faabf07d18
tavassoliachi <87390570+tavassoliachi@users.noreply.github.com>
2022-01-06 19:17:25 +0200

    Add files via upload

---
+++ src/route/Checkout/Checkout.component.js
@@ -0,0 +1,38 @@
+import ContentWrapper from "Component/ContentWrapper";
+import {
+    Checkout as CheckoutMain
+} from "SourceRoute/Checkout/Checkout.component";
+import ProgressBar from "Component/ProgressBar";
+export default class Checkout extends CheckoutMain {
+    generateProgressBar(){
+        const { checkoutStep } = this.props;
+        return <ProgressBar step={checkoutStep}/>
+    }
+    render() {
+        const {checkoutStep} = this.props
+        console.log(checkoutStep,"-",window.location)
+        return (
+            <main block="Checkout">
+                {this.generateProgressBar()}
+                <ContentWrapper
+                  wrapperMix={ { block: 'Checkout', elem: 'Wrapper' } }
+                  label={ __('Checkout page') }
+                >
+                    { this.renderSummary(true) }
+                    <div block="Checkout" elem="Step">
+                        { this.renderTitle() }
+                        { this.renderGuestForm() }
+                        { this.renderStep() }
+                        { this.renderLoader() }
+                    </div>
+                    <div>
+                        { this.renderSummary() }
+                        { this.renderPromo() }
+                        { this.renderCoupon() }
+                    </div>
+                </ContentWrapper>
+            </main>
+        )
+    }
+}
+    
